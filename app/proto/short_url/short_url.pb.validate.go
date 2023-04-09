020c08391b697ef3a4564c8ca479ccb828d21b03
3FanYu <eric93504@gmail.com>
2023-04-09 22:47:05 +0800

    fix: update protobuf

--- app/proto/short_url/short_url.pb.validate.go
+++ app/proto/short_url/short_url.pb.validate.go
@@ -61,35 +61,6 @@ func (m *ShortUrl) validate(all bool) error {
 
 	// no validation rules for Url
 
-	if all {
-		switch v := interface{}(m.GetCreatedAt()).(type) {
-		case interface{ ValidateAll() error }:
-			if err := v.ValidateAll(); err != nil {
-				errors = append(errors, ShortUrlValidationError{
-					field:  "CreatedAt",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		case interface{ Validate() error }:
-			if err := v.Validate(); err != nil {
-				errors = append(errors, ShortUrlValidationError{
-					field:  "CreatedAt",
-					reason: "embedded message failed validation",
-					cause:  err,
-				})
-			}
-		}
-	} else if v, ok := interface{}(m.GetCreatedAt()).(interface{ Validate() error }); ok {
-		if err := v.Validate(); err != nil {
-			return ShortUrlValidationError{
-				field:  "CreatedAt",
-				reason: "embedded message failed validation",
-				cause:  err,
-			}
-		}
-	}
-
 	if len(errors) > 0 {
 		return ShortUrlMultiError(errors)
 	}
